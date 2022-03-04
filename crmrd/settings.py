15673d7598113711933571dff11becc4b9fbe9bc
Daniel J Williams <kooltechsolutions101@gmail.com>
2022-03-04 16:26:22 -0400

    update for uncommitted file

--- crmrd/settings.py
+++ crmrd/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = "media_root"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = "static_root"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'leads.User'
@@ -182,7 +182,6 @@ LOGGING = {
         'level': 'WARNING',
     },
 }
-
 TAILWIND_APP_NAME = 'theme'
 
 
